97cba6534f59c8d2ab6a8c517e556ca6b0ab7eef
codedbyminjae <minjaekim7111@naver.com>
2025-11-19 22:02:02 +0900

    feat: add Tile data model for collage engine structure

---
+++ src/tile.py
@@ -0,0 +1,7 @@
+class Tile:
+    def __init__(self, img_path, hue, saturation, brightness, edge_destiny):
+        self.img_path = img_path
+        self.hue = hue
+        self.saturation = saturation
+        self.brightness = brightness
+        self.edge_destiny = edge_destiny
