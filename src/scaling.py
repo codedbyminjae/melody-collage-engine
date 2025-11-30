5462a78e1c62c561e45f1165f28fb6c574db75ca
codedbyminjae <minjaekim7111@naver.com>
2025-11-30 19:37:49 +0900

    이미지 밝기 기반 정규화 과정 Final

--- src/scaling.py
+++ src/scaling.py
@@ -1,20 +1,21 @@
-def compute_scale(brightness_values, min_scale=0.7, max_scale=1.8):
+def compute_scale(brightness_list, min_scale=0.7, max_scale=1.8):
+
     # brightness 최소/최대 계산
-    b_min = min(brightness_values)
-    b_max = max(brightness_values)
+    b_min = min(brightness_list)
+    b_max = max(brightness_list)
 
-    scale_factors = []
+    scale_list = []
 
-    for b in brightness_values:
-        # brightness -> 0~1 정규화
-        if b_max == b_min:
-            norm = 0.5      # 값이 모두 같을 때
+    for b in brightness_list:
+        # brightness 0~1사이의 값으로 정규화
+        if b_max == b_min: # 값이 같을때의 예외처리 (분모가 0인 경우)
+            norm = 0.5
         else:
             norm = (b - b_min) / (b_max - b_min)
 
-        # 정규화값을 scale 범위로 매핑
+        # 정규화값을 scale 범위로 매핑 (0.7 ~ 1.8 사이)
         scale = min_scale + norm * (max_scale - min_scale)
-        scale_factors.append(scale)
+        scale_list.append(scale)
 
-    # segment별 scale factor 리스트 반환
-    return scale_factors
+    # segment별 scale 리스트 반환
+    return scale_list
